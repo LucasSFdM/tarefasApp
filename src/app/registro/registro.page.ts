5a74dbabdfc44a1a4d046e22ab00c2baa10ca2fe
Lucas Santiago <lsfdm09082002@gmail.com>
2020-08-26 14:32:49 -0300

    Validacao de formularios

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CpfValidator } from '../validators/cpf-validator';
+import { ComparaValidator } from '../validators/comparacao-validator';
 
 
 @Component({
@@ -18,20 +20,22 @@ export class RegistroPage implements OnInit {
     ],
     senha1: [
       { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
-      { tipo: 'minLength', mensagem: 'A senha deve ter pelo menos 6 caracteres!!' }
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres!!' }
     ],
     senha2: [
       { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
-      { tipo: 'minLength', mensagem: 'A senha deve ter pelo menos 6 caracteres!!' }
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres!!' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a senha!' }
     ],
     nome: [
       { tipo: 'required', mensagem: 'O campo nome é obrigatório!' },
-      { tipo: 'minLength', mensagem: 'O nome deve ter pelo menos 3 caracteres!!' }
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres!!' }
     ],
     cpf: [
       { tipo: 'required', mensagem: 'O CPF é obrigatório!' },
-      { tipo: 'minLength', mensagem: 'O CPF deve ter no mínimo 11 caracteres!!' },
-      { tipo: 'maxLength', mensagem: 'O CPF deve ter no máximo 14 caracteres!!' }
+      { tipo: 'minlength', mensagem: 'O CPF deve ter no mínimo 11 caracteres!!' },
+      { tipo: 'maxlength', mensagem: 'O CPF deve ter no máximo 14 caracteres!!' },
+      { tipo: 'invalido', mensagem: 'CPF inválido!' }
     ],
     datanascimento: [
       { tipo: 'required', mensagem: 'A data de nascimento é obrigatória!' },
@@ -40,7 +44,8 @@ export class RegistroPage implements OnInit {
       { tipo: 'required', mensagem: 'O genero é obrigatório!' },
     ],
     celular: [
-      { tipo: 'maxLength', mensagem: 'O celular deve ter no máximo 16 caracteres!!' }
+      { tipo: 'minlength', mensagem: 'O celular deve ter no mínimo 10 caracteres!!' },
+      { tipo: 'maxlength', mensagem: 'O celular deve ter no máximo 16 caracteres!!' }
     ]
   };
 
@@ -50,10 +55,14 @@ export class RegistroPage implements OnInit {
       senha1: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
       senha2: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14)])],
+      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14),
+      CpfValidator.cpfValido
+      ])],
       datanascimento: ['', Validators.compose([Validators.required])],
       genero: ['', Validators.compose([Validators.required])],
-      celular: ['', Validators.compose([Validators.maxLength(14)])]
+      celular: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(14)])]
+    },{
+      validator: ComparaValidator('senha1', 'senha2')
     });
   }
 
